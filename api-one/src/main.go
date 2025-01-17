a6382f2c384751d928c107ecf20cde9ee1320016
vasantbala <30379760+vasantbala@users.noreply.github.com>
2025-01-17 23:05:02 +0000

    2 apis and 1 ui

---
+++ api-one/src/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+    "fmt"
+    "net/http"
+)
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "This is API 1!")
+}
+
+func main() {
+    http.HandleFunc("/", helloHandler)
+    fmt.Println("Starting server at port 8080")
+    if err := http.ListenAndServe(":8080", nil); err != nil {
+        fmt.Println(err)
+    }
+}
