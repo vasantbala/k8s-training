18538612aeb4536cc8f21e8910a9b7720267d984
vasantbala <30379760+vasantbala@users.noreply.github.com>
2025-01-19 16:03:01 +0000

    fix build error

--- ui-one/src/api/apipage.tsx
+++ ui-one/src/api/apipage.tsx
@@ -29,7 +29,7 @@ function ApiPage() {
             .then((response) => setApiTwoResult(response.data))
             .catch((error) => {
                 console.error("Error fetching data:", error);
-                setApiOneResult("Error fetching data");
+                setApiTwoResult("Error fetching data");
             });
     }, [api_two_url]);
 
