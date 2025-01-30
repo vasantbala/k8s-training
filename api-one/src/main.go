f8aa401582c9527e8759657d7c2aba2e0187af8c
Vasant Balasubramanian <vasant.balasubramanian@sap.com>
2025-01-29 22:06:06 -0600

    fix cors issue on api-one and api-two

--- api-one/src/main.go
+++ api-one/src/main.go
@@ -1,18 +1,23 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
+	"fmt"
+	"net/http"
 )
 
 func helloHandler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "This is API 1!")
+	enableCors(&w)
+	fmt.Fprintf(w, "This is API 1!")
 }
 
 func main() {
-    http.HandleFunc("/", helloHandler)
-    fmt.Println("Starting server at port 8080")
-    if err := http.ListenAndServe(":8080", nil); err != nil {
-        fmt.Println(err)
-    }
+	http.HandleFunc("/", helloHandler)
+	fmt.Println("Starting server at port 8080")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		fmt.Println(err)
+	}
+}
+
+func enableCors(w *http.ResponseWriter) {
+	(*w).Header().Set("Access-Control-Allow-Origin", "*")
 }
